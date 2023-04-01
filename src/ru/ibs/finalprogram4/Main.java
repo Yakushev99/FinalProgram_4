c4c9b14094cf1699ef5ae5d5453e50d3e3995da1
MYAkushev <m.yakushev.99@gmail.com>
2023-04-02 00:00:05 +0500

    Commit FinalProgram_4

---
+++ src/ru/ibs/finalprogram4/Main.java
@@ -0,0 +1,38 @@
+package ru.ibs.finalprogram4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        for (int i = 0; i<3; i++) {
+            System.out.println("Введите ответ: ");
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String str = input.readLine();
+            if (i == 0){
+                if (str.equals("Подсказка")) {
+                    System.out.println("Сжатый Covid для компьтера");
+                }
+            }
+            else if (i > 0 && str.equals("Подсказка")){
+                System.out.println("Подсказка уже недоступна");
+                i = 1;
+            }
+            if (!str.equals("Заархивированный вирус")) {
+                if (i < 2 && !str.equals("Подсказка")) {
+                    System.out.println("Подумай еще!");
+                }
+                else if (i == 2) {
+                    System.out.println("Обидно, приходи в другой раз");
+                    break;
+                }
+            }
+            else {
+                System.out.println("Правильно!");
+                break;
+            }
+        }
+    }
+}
